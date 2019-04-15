efa6125a01ccc1f79703bf0c0c92123663513383
shihuaxian <910943466@qq.com>
2019-04-15 17:57:56 +0800

    update stack to queue

--- src/com/cn/passself/normal/PairUtil.java
+++ src/com/cn/passself/normal/PairUtil.java
@@ -31,6 +31,20 @@ public class PairUtil {
     public static void main(String[] args) {
 
     }
+
+    public interface Comparable<T>{
+        public int compareTo(T o);
+    }
+
+    public static <T extends Comparable<T>> int countGreaterThan(T[] anArray,T elem){
+        int count = 0;
+        for (T e:anArray){
+            if (e.compareTo(elem) >0){
+                ++count;
+            }
+        }
+        return count;
+    }
 }
 
 
